0cf98ac02d30fb54fcb57a95650d4663f3248f34
Arpit Thappa <arpitthaapa8@gmail.com>
2024-12-26 20:42:47 +0530

    CylinderCrousel project

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {Canvas} from "@react-three/fiber";
+import "./style.css";
+import { Cylinder, OrbitControls} from '@react-three/drei';
+import  * as THREE from "three";
+import Cyl from "./Cyl";
+import {Bloom, EffectComposer, ToneMapping} from "@react-three/postprocessing";
+
+const App = () => {
+  return(
+    <>
+   <Canvas flat camera={{fov:35}}>
+    <OrbitControls />
+    <ambientLight/>
+    <Cyl />
+    <EffectComposer>
+< Bloom
+mipmapBlur
+    intensity={6} // The bloom intensity.
+    luminanceThreshold={0} 
+    luminanceSmoothing={0} 
+  />
+  </EffectComposer>
+    
+   </Canvas>
+  
+  
+
+  
+   <div className="w-full bg-black py-32">
+    <h1 className="text-white">Hows this !!</h1>
+     </div>
+  </> 
+   );
+  
+};
+
+export default App;
+
+
+
+// npm run dev
